a60673f39d0773724701973835b930b990f7612c
Nick-Hericks <nickhericks@gmail.com>
2019-05-29 18:56:15 +0200

    Update getUsers comments

--- app.js
+++ app.js
@@ -8,6 +8,37 @@ app.set('view engine', 'pug');
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static('public'));
 
+
+
+
+
+
+
+
+//CALL BACKS
+function getUsers(cb){
+	// make request
+	fs.readFile('data.json', 'utf8', (err, data) => {
+		// if error, return the callback function with error as argument
+		if (err) return cb(err);
+		// If error, none of the below code is executed
+		// parse response data and assign to variable
+		const users = JSON.parse(data);
+		// return callback function
+		// if we're returning this, it means there was no error, so
+		// we pass 'null' as the first argument
+		// we pass our 'users' data as the second argument
+		// the callback function then takes these arguments
+		// and executes the code in the callback
+		return cb(null, users);
+	});
+}
+
+
+
+
+
+
 //CALL BACKS
 function getUsers(cb){
   fs.readFile('data.json', 'utf8', (err, data) => {
